5996bf82474bd9e22ab80c1bc6bf93c2573b701e
MFQ <softmfq@gmail.com>
2015-08-03 22:56:08 +0500

    init package

---
+++ package.js
@@ -0,0 +1,22 @@
+Package.describe({
+  name: 'mfq:account-modal',
+  version: '0.0.1',
+  // Brief, one-line summary of the package.
+  summary: '',
+  // URL to the Git repository containing the source code for this package.
+  git: '',
+  // By default, Meteor will default to using README.md for documentation.
+  // To avoid submitting documentation, set this field to null.
+  documentation: 'README.md'
+});
+
+Package.onUse(function(api) {
+  api.versionsFrom('1.1.0.2');
+  api.addFiles('account-modal.js');
+});
+
+Package.onTest(function(api) {
+  api.use('tinytest');
+  api.use('mfq:account-modal');
+  api.addFiles('account-modal-tests.js');
+});
